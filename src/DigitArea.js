2262ce1732af3762136137a6d5622c2804f26329
Albert Stjärne <albert.stjarne@gmail.com>
2020-02-24 16:47:37 +0100

    adds onClick event saving pressed digit to state in DigitArea

--- src/DigitArea.js
+++ src/DigitArea.js
@@ -11,6 +11,15 @@ class DigitArea extends Component {
       digitAreaKeys: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '', '', '', '', '', ''],
       enteredCode: [],
     };
+
+    this.saveDigitInput = this.saveDigitInput.bind(this);
+  }
+
+  // saving entered digit to state
+  saveDigitInput(receivedDigit) {
+    this.setState(prevState => ({
+      enteredCode: [...prevState.enteredCode, receivedDigit],
+    }));
   }
 
   render() {
@@ -23,7 +32,13 @@ class DigitArea extends Component {
             if (digit === '') {
               return <NonDigitButton key={id} digit={digit}></NonDigitButton>;
             } else {
-              return <DigitButton key={id} digit={digit}></DigitButton>;
+              return (
+                <DigitButton
+                  key={id}
+                  digit={digit}
+                  saveDigit={this.saveDigitInput}
+                ></DigitButton>
+              );
             }
           })}
         </div>
--- src/DigitButton.js
+++ src/DigitButton.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import './DigitButton.css';
 
 class DigitButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.sendCode = this.sendCode.bind(this);
+  }
+
+  // sending entered code to parent
+  sendCode() {
+    this.props.saveDigit(this.props.digit);
+  }
+
   render() {
     return (
       <div className='DigitButton'>
-        <button className='digitButton'>{this.props.digit}</button>
+        <button className='digitButton' onClick={this.sendCode}>
+          {this.props.digit}
+        </button>
       </div>
     );
   }
